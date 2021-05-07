6586ae43d86a6fcedb7ffbfaaee8e53eccdd10ed
Thomas Thornton <>
2021-05-07 10:21:20 +0100

    Remove RG for now

--- terraform/aks/05-mis/application-managed-identity-rg.tf
+++ terraform/aks/05-mis/application-managed-identity-rg.tf
@@ -1,5 +1,6 @@
-resource "azurerm_resource_group" "application-mi" {
-  name     = "managed-identities-${var.environment}-rg"
-  location = var.location
-  tags     = local.common_tags
-}
+# # Not needed for CFT, we currently use managed-identities-${var.environment}-rg to store all the application MI's
+# resource "azurerm_resource_group" "application-mi" {
+#   name     = "managed-identities-${var.environment}-rg"
+#   location = var.location
+#   tags     = local.common_tags
+# }
