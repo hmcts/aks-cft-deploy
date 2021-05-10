83c85e035691cfff89de83c6b7a042dc6fbf064f
Thomas Thornton <>
2021-05-10 11:41:25 +0100

    Deploy cluster admin roles

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -29,11 +29,12 @@ fi
 
 chmod +x get-aks-credentials.sh
 chmod +x create-custom-namespaces.sh
+chmod +x create-cluster-admins.sh
 
 echo "Starting Deployment"
 ./get-aks-credentials.sh "$@" || error_exit "ERROR: Unable to get AKS credentials"
-echo "Custom Namespaces"
 ./create-custom-namespaces.sh "$@" || error_exit "ERROR: Unable to create custom namespaces"
+./create-cluster-admins.sh || error_exit "ERROR: Unable to create cluster admins"
 # ./create-sshkeys.sh "$@" || error_exit "ERROR: SSHKey Create Issues"
 # ./apply-default-rbac.sh "$@" || error_exit "ERROR: Unable to set k8s RBAC"
 # ./deploy-flux.sh "$@" || error_exit "ERROR: Unable to deploy Fluxcd"
