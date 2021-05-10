a0bbcd7645bc48f9898683226cfe8ad737dc0b8f
Thomas Thornton <>
2021-05-10 14:44:08 +0100

    test

--- bootstrap/install-sealed-secrets.sh
+++ bootstrap/install-sealed-secrets.sh
@@ -5,14 +5,16 @@ VAULT_NAME=${4}
 VERSION="1.10.3"
 NAMESPACE="admin"
 
-az keyvault secret download \
-  --file sealed-secrets-pki.yaml \
-  --name sealed-secrets-pki \
-  --encoding ascii \
-  --vault-name $VAULT_NAME
+echo "KEY VAULT IS $VAULT_NAME"
 
-kubectl apply -f sealed-secrets-pki.yaml
+# az keyvault secret download \
+#   --file sealed-secrets-pki.yaml \
+#   --name sealed-secrets-pki \
+#   --encoding ascii \
+#   --vault-name $VAULT_NAME
 
-helm repo add hmctspublic https://hmctspublic.azurecr.io/helm/v1/repo
-helm upgrade sealed-secrets hmctspublic/sealed-secrets --version 1.12.0 --install --namespace ${NAMESPACE} \
-     -f  deployments/sealed-secrets/values.yaml --set secretName=sealed-secrets-pki --wait \
+# kubectl apply -f sealed-secrets-pki.yaml
+
+# helm repo add hmctspublic https://hmctspublic.azurecr.io/helm/v1/repo
+# helm upgrade sealed-secrets hmctspublic/sealed-secrets --version 1.12.0 --install --namespace ${NAMESPACE} \
+#      -f  deployments/sealed-secrets/values.yaml --set secretName=sealed-secrets-pki --wait \
