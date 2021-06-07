b233c8e1ac746ec38ff9354a9f0ebf57219fb2df
Avi <avinash.mutham@hmcts.net>
2021-06-07 12:25:59 +0100

    checking format

--- components/05-mis/11-jenkins.tf
+++ components/05-mis/11-jenkins.tf
@@ -5,7 +5,7 @@ locals {
 
     cftptl = {
       subscription = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
-      mi = "jenkins-cftptl-intsvc-mi"
+      mi           = "jenkins-cftptl-intsvc-mi"
     }
     cftsbox = {
       subscription = "1497c3d7-ab6d-4bb7-8a10-b51d03189ee3"
