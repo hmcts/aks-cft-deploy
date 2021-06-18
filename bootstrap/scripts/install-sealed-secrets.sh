272668e4dd2f408fdc44c72519f553e336a6f076
Thomas Thornton <>
2021-06-18 10:27:09 +0100

    if sealedsecrets file exists

--- bootstrap/scripts/install-sealed-secrets.sh
+++ bootstrap/scripts/install-sealed-secrets.sh
@@ -5,11 +5,13 @@ VAULT_NAME=${8}
 VERSION="1.10.3"
 NAMESPACE="admin"
 
+if [ -f sealed-secrets-pki.yaml ]; then
 az keyvault secret download \
   --file sealed-secrets-pki.yaml \
   --name sealed-secrets-pki \
   --encoding ascii \
   --vault-name $VAULT_NAME
+fi
 
 kubectl apply -f sealed-secrets-pki.yaml
 
