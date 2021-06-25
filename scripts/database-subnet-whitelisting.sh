75c521507cb40c543d17ec29e620a3dbff67dc0f
Avi <avinash.mutham@hmcts.net>
2021-06-25 17:05:36 +0100

    changes for making env specific

--- scripts/database-subnet-whitelisting.sh
+++ scripts/database-subnet-whitelisting.sh
@@ -1,7 +1,8 @@
 set -e
 
-JSONPATH="[?(contains(type, 'Microsoft.DBforPostgreSQL/servers') && contains(name, 'perftest'))].name"
-POSTGRES_LIST=$(az resource list --query "${JSONPATH}" --subscription="" -o tsv | sort -u)
+env="perftest"
+
+POSTGRES_LIST=$(az postgres server list --subscription="DCD-CNP-QA" --query "[?(contains(name,'$env'))].name" -o tsv | sort -u)
 
 for pg in ${POSTGRES_LIST[@]}
 do
@@ -11,18 +12,18 @@ do
         rg=${pg//-idam/-data-idam}
         ;;
 
-    *"-perftest-clone"*)
-        rg=${pg//-perftest-clone/-data-perftest}
+    *"-$env-clone"*)
+        rg=${pg//-$env-clone/-data-$env}
         ;;
     
-    *"-perftest"*)
-        rg=${pg//-perftest/-data-perftest}
+    *"-$env"*)
+        rg=${pg//-$env/-data-$env}
         ;;
 
     esac
     if [ "$(az postgres server vnet-rule list --resource-group $rg --server-name $pg -o tsv)" ]; 
     then 
-        echo "Skipping this as it already has vnet rules"; 
+        echo "Skipping this as $pg already has vnet rules"; 
     else 
         echo "Creating vnet rules for $pg"; 
         az postgres server vnet-rule create --name=cft-aks-00 --subnet /subscriptions/8a07fdcd-6abd-48b3-ad88-ff737a4b9e3c/resourceGroups/cft-test-network-rg/providers/Microsoft.Network/virtualNetworks/cft-test-vnet/subnets/aks-00  --resource-group $rg --server-name $pg
