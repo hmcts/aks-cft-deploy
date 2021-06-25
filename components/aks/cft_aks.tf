dcd94cebaf30b437cf415d84b033f25df1e30bf5
Thomas Thornton <>
2021-06-25 08:47:42 +0100

    no message

--- components/aks/cft_aks.tf
+++ components/aks/cft_aks.tf
@@ -13,15 +13,15 @@ resource "azurerm_role_assignment" "uami_cft_rg_identity_operator" {
   depends_on = [module.kubernetes]
 }
 
-data "azurerm_resource_group" "managed-identity-operator" {
+data "azurerm_resource_group" "managed-identity-operator-cft" {
   name = "managed-identities-${local.environment-mi}-rg"
 
 }
 
-resource "azurerm_role_assignment" "uami_rg_identity_operator" {
+resource "azurerm_role_assignment" "uami_cft_rg_identity_operator2" {
   count                = var.cluster_count
   principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
-  scope                = data.azurerm_resource_group.managed-identity-operator.id
+  scope                = data.azurerm_resource_group.managed-identity-operator-cft.id
   role_definition_name = "Managed Identity Operator"
 
   depends_on = [module.kubernetes]
