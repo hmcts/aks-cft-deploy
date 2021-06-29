1628c9dc64d37c125a7c9625f013f7f9d76c5c77
avi-hmcts <71495456+avi-hmcts@users.noreply.github.com>
2021-06-29 13:36:12 +0100

    Revert provider changes (#49)
    * script to add vnet rules
    
    * to fix destroy errors on pipeline
    
    * Clean up
    
    * formatting issues

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -62,9 +62,9 @@ provider "azurerm" {
   subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
 }
 
-# provider "azurerm" {
-#   alias                      = "mi_cft"
-#   skip_provider_registration = "true"
-#   features {}
-#   subscription_id = local.mi_cft[var.environment].subscription
-# }
+provider "azurerm" {
+  alias                      = "mi_cft"
+  skip_provider_registration = "true"
+  features {}
+  subscription_id = local.mi_cft[var.environment].subscription
+}
