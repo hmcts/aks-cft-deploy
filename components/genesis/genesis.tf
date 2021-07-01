5ef974401cf8a57d106f94956571f19b81747381
Avi <avinash.mutham@hmcts.net>
2021-07-01 10:59:18 +0100

    Fixing pipeline errors

--- components/genesis/genesis.tf
+++ components/genesis/genesis.tf
@@ -1,6 +1,12 @@
+locals {
+
+  environment = (var.environment == "perftest") ? "test" : "${var.environment}"
+
+}
+
 module "genesis" {
-  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=master"
-  environment      = var.environment
+  source           = "git::https://github.com/hmcts/aDTS Operationsks-module-genesis.git?ref=master"
+  environment      = local.environment
   tags             = module.ctags.common_tags
   developers_group = local.developers_group
 }
