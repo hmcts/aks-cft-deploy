664d69cd8ba08531968af508eb910f9a9bae07e7
avi-hmcts <71495456+avi-hmcts@users.noreply.github.com>
2021-07-01 17:48:50 +0100

    fixing azure vault control access (#55)
    * changing naming to perftest
    
    * to pick the right serviceConnection
    
    * correcting format
    
    * correcting format
    
    * Fixing pipeline errors for perftest
    
    * fixing formatting
    
    * Conditionals to pick the right Azure control rg
    
    * Fixing typos
    
    * Fixing pipeline errors
    
    * Correcting the conditional check
    
    * Syntax checks
    
    * Fixing pipeline errors
    
    * Fixing pipeline errors
    
    * Fixing pipeline errors
    
    * Fixing pipeline errors
    
    * fixing template errors
    
    * Fixing pipeline errors
    
    * fixing template errors
    
    * fixing template errors
    
    * Fixing pipeline errors
    
    * fixing tf apply issues
    
    * updating kubernetes branch reference

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -23,7 +23,7 @@ module "loganalytics" {
 
 module "kubernetes" {
   count  = var.cluster_count
-  source = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=master"
+  source = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=control-vault-access"
 
   environment = var.environment
   location    = var.location
