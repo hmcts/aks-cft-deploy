7a295a37b12c34bdf3f0404ba5da9275bbb10daa
Avi <avinash.mutham@hmcts.net>
2021-07-01 11:26:41 +0100

    fixing template errors

--- components/network/interpolated-defaults.tf
+++ components/network/interpolated-defaults.tf
@@ -3,9 +3,12 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 locals {
+
+  environment = (var.environment == "perftest") ? "test" : "${var.environment}"
+
   network_resource_group_name = format("%s-%s-network-rg",
     var.project,
-    var.environment
+    local.environment
   )
   network_shortname = format("%s_%s",
     var.project,
