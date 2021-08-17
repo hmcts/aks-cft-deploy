901f4d822de418422cd7d00c5d720057c29bf23c
Darren Bromwich <d.bromwich@kainos.com>
2021-08-17 12:00:46 +0100

    added route table

---
+++ components/network/routetable.tf
@@ -0,0 +1,22 @@
+resource "azurerm_route_table" "routetable" {
+  name                = "aks-aat-core-infra-route-table"
+  location            = var.location
+  resource_group_name = core-infra-var.environment
+
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "route1"
+    address_prefix = "10.1.0.0/16"
+    next_hop_type  = "vnetlocal"
+  }
+
+  tags = module.ctags.common_tags
+}
+
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.environment
+  product     = var.product
+  builtFrom   = var.builtFrom
+}
