9eb6843f40aab192f773006f5ddbb921fe72b406
Darren Bromwich <d.bromwich@kainos.com>
2021-08-17 12:26:18 +0100

    removed tags module

--- components/network/routetable.tf
+++ components/network/routetable.tf
@@ -21,10 +21,3 @@ resource "azurerm_route_table" "routetable" {
 
   tags = module.ctags.common_tags
 }
-
-module "ctags" {
-  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = var.environment
-  product     = var.product
-  builtFrom   = var.builtFrom
-}
