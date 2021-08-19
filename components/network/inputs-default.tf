84248fb0654ecbb34b3be1d6ef96f50306e63cc3
Darren Bromwich <d.bromwich@kainos.com>
2021-08-19 14:39:54 +0100

    removed un-used vars

--- components/network/inputs-default.tf
+++ components/network/inputs-default.tf
@@ -98,14 +98,3 @@ variable "additional_routes_appgw" {
 variable "routes" {
   default = []
 }
-variable "route_address_prefix" {
-  default = "0.0.0.0/0"
-}
-
-variable "route_next_hop_type" {
-  default = "VirtualAppliance"
-}
-
-variable "route_next_hop_in_ip_address" {
-  default = "10.10.1.1"
-}
