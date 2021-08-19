02d4556f549f587f245d55579bc46b43ea108086
Darren Bromwich <d.bromwich@kainos.com>
2021-08-19 14:54:17 +0100

    removed additional_route resource

--- components/network/routetable.tf
+++ components/network/routetable.tf
@@ -23,16 +23,3 @@ resource "azurerm_route" "default_route" {
 
   provider = azurerm.core-infra-routetable
 }
-
-#resource "azurerm_route" "additional_route" {
-#  for_each = { for route in var.additional_routes : route.name => route }
-
-#  name                   = lower(each.value.name)
-#  route_table_name       = azurerm_route_table.route_table.name
-#  resource_group_name    = "core-infra-${var.environment}"
-#  address_prefix         = each.value.address_prefix
-#  next_hop_type          = each.value.next_hop_type
-#  next_hop_in_ip_address = each.value.next_hop_type != "VirtualAppliance" ? null : each.value.next_hop_in_ip_address
-
-#  provider = azurerm.core-infra-routetable
-#}
