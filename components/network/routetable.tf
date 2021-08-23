0862b8b38beee799600d5636057054df6ef828c8
Thomas Thornton <>
2021-08-23 12:58:49 +0100

    whoops - removed wrong resource

--- components/network/routetable.tf
+++ components/network/routetable.tf
@@ -24,11 +24,13 @@ resource "azurerm_route" "coreinfra_routes" {
   provider = azurerm.core-infra-routetable
 }
 
-resource "azurerm_subnet_route_table_association" "coreinfra_subnets" {
-  for_each = { for subnet_id in var.coreinfra_subnets : subnet_id.subnet_id => subnet_id }
+data "azurerm_subnet" "coreinfra_subnets" {
+  for_each = { for subnet in var.coreinfra_subnets : subnet.name => subnet }
 
-  route_table_id = azurerm_route_table.route_table_coreinfra.id
-  subnet_id      = each.value.subnet_id
+  name                 = each.value.name
+  virtual_network_name = "core-infra-vnet-${var.environment}"
+  resource_group_name  = "core-infra-${var.environment}"
+  provider             = azurerm.core-infra-routetable
 }
 
 resource "azurerm_subnet_route_table_association" "coreinfra_subnets" {
