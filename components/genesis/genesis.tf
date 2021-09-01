f1380b0756d7e98b375ec41afcab260cfa35fe41
Darren Bromwich <d.bromwich@kainos.com>
2021-09-01 14:08:12 +0100

    changed genesis branch for testing fix

--- components/genesis/genesis.tf
+++ components/genesis/genesis.tf
@@ -1,5 +1,5 @@
 module "genesis" {
-  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=DTSPO-1032-ptlsbox"
+  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=dtspo4185-fix-azuread-errors"
   environment      = local.environment
   tags             = module.ctags.common_tags
   developers_group = local.developers_group
