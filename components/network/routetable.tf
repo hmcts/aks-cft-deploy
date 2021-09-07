998ffba2b4d66cbbbcb27ccad7df2ebee4083bc5
Darren Bromwich <d.bromwich@kainos.com>
2021-09-07 20:24:26 +0100

    update to route table

--- components/network/routetable.tf
+++ components/network/routetable.tf
@@ -4,11 +4,6 @@ resource "azurerm_route_table" "route_table_coreinfra" {
     var.environment
   )
 
-  for_each = {
-    for ptlsbox in var.environment : ptlsbox => v
-    if ptlsbox.name != ""
-  }
-
   provider = azurerm.core-infra-routetable
 
   location            = var.location
