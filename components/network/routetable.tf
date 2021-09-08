c34187e9a8d0e5a727717e81f0b495e6f03bef14
Darren Bromwich <d.bromwich@kainos.com>
2021-09-08 17:17:50 +0100

    testing route table

--- components/network/routetable.tf
+++ components/network/routetable.tf
@@ -1,6 +1,5 @@
 resource "azurerm_route_table" "route_table_coreinfra" {
-  count = var.environment == "PTLSBOX" ? 0 : 0 == "SBOX" ? 0 : 1
-
+  count = var.environment == "PTLSBOX" ? 0 : 1
   name = format("%s-%s-core-infra-route-table",
     var.service_shortname,
     var.environment
@@ -13,40 +12,40 @@ resource "azurerm_route_table" "route_table_coreinfra" {
   tags                = module.ctags.common_tags
 }
 
-# resource "azurerm_route" "coreinfra_routes" {
-#   for_each = { for route in var.additional_routes_coreinfra : route.name => route }
+resource "azurerm_route" "coreinfra_routes" {
+  for_each = { for route in var.additional_routes_coreinfra : route.name => route }
 
-#   name                   = lower(each.value.name)
-#   route_table_name       = azurerm_route_table.route_table_coreinfra.name
-#   resource_group_name    = "core-infra-${local.environment}"
-#   address_prefix         = each.value.address_prefix
-#   next_hop_type          = each.value.next_hop_type
-#   next_hop_in_ip_address = each.value.next_hop_in_ip_address
+  name                   = lower(each.value.name)
+  route_table_name       = azurerm_route_table.route_table_coreinfra.name
+  resource_group_name    = "core-infra-${local.environment}"
+  address_prefix         = each.value.address_prefix
+  next_hop_type          = each.value.next_hop_type
+  next_hop_in_ip_address = each.value.next_hop_in_ip_address
 
-#   provider = azurerm.core-infra-routetable
+  provider = azurerm.core-infra-routetable
 
-#   depends_on = [
-#     azurerm_route_table.route_table_coreinfra
-#   ]
-# }
+  depends_on = [
+    azurerm_route_table.route_table_coreinfra
+  ]
+}
 
-# data "azurerm_subnet" "coreinfra_subnets" {
-#   for_each = { for subnet in var.coreinfra_subnets : subnet.name => subnet }
+data "azurerm_subnet" "coreinfra_subnets" {
+  for_each = { for subnet in var.coreinfra_subnets : subnet.name => subnet }
 
-#   name                 = each.value.name
-#   virtual_network_name = "core-infra-vnet-${local.environment}"
-#   resource_group_name  = "core-infra-${local.environment}"
-#   provider             = azurerm.core-infra-routetable
-# }
+  name                 = each.value.name
+  virtual_network_name = "core-infra-vnet-${local.environment}"
+  resource_group_name  = "core-infra-${local.environment}"
+  provider             = azurerm.core-infra-routetable
+}
 
-# resource "azurerm_subnet_route_table_association" "coreinfra_subnets" {
-#   for_each = { for subnet in var.coreinfra_subnets : subnet.name => subnet }
+resource "azurerm_subnet_route_table_association" "coreinfra_subnets" {
+  for_each = { for subnet in var.coreinfra_subnets : subnet.name => subnet }
 
-#   route_table_id = azurerm_route_table.route_table_coreinfra.id
-#   subnet_id      = data.azurerm_subnet.coreinfra_subnets[each.value.name].id
-#   provider       = azurerm.core-infra-routetable
+  route_table_id = azurerm_route_table.route_table_coreinfra.id
+  subnet_id      = data.azurerm_subnet.coreinfra_subnets[each.value.name].id
+  provider       = azurerm.core-infra-routetable
 
-#   depends_on = [
-#     azurerm_route_table.route_table_coreinfra
-#   ]
-# }
+  depends_on = [
+    azurerm_route_table.route_table_coreinfra
+  ]
+}
