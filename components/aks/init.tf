30419326285ac0f77d69177ee3b912d49b640a4e
Darren Bromwich <d.bromwich@kainos.com>
2021-09-16 13:55:20 +0100

    removed unused var and update tfvars for ptlsbox

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -24,8 +24,6 @@ locals {
   environment = var.environment == "sbox" ? "sandbox" : var.environment == "test" ? "perftest" : var.environment == "ptlsbox" ? "cftsbox-intsvc" : "${var.environment}"
 
   environment-mi = var.environment == "sandbox" ? "sbox" : var.environment == "test" ? "perftest" : var.environment == "aat" ? "stg" : "${var.environment}"
-
-  kubernetes-environment = var.environment == "ptlsbox" ? "sbox" : "${var.environment}"
   acr = {
     global = {
       subscription = "8999dec3-0104-4a27-94ee-6588559729d1"
