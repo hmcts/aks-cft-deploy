e256a4e59616dc59288ad18a312de33f1eb4fe9b
Darren Bromwich <d.bromwich@kainos.com>
2021-10-01 10:16:15 +0100

    added PTL subscription to init

--- components/network/init.tf
+++ components/network/init.tf
@@ -97,6 +97,9 @@ locals {
     ptlsbox = {
       subscription = "1497c3d7-ab6d-4bb7-8a10-b51d03189ee3"
     }
+    ptl = {
+      subscription = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+    }
   }
 
   environment = var.environment == "sbox" ? "sandbox" : var.environment == "test" ? "perftest" : "${var.environment}"
