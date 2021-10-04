7594f45e84df1981771b200ba216d701269978d2
Darren Bromwich <d.bromwich@kainos.com>
2021-10-04 10:16:51 +0100

    update to network component

--- components/network/network.tf
+++ components/network/network.tf
@@ -1,8 +1,6 @@
 module "network" {
   source = "git::https://github.com/hmcts/aks-module-network.git?ref=cft"
 
-  for_each = toset(var.hub)
-
   resource_group_name = local.network_resource_group_name
 
   route_next_hop_in_ip_address = local.hub[var.hub].ukSouth.next_hop_ip
