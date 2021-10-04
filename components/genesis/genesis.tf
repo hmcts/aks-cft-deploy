431a01f6df50462635f72a8141ec3173b140e053
Thomas Thornton <>
2021-10-04 13:17:52 +0100

    test project on genesis

--- components/genesis/genesis.tf
+++ components/genesis/genesis.tf
@@ -1,8 +1,9 @@
 module "genesis" {
-  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=master"
+  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=cftsds"
   environment      = local.environment
   tags             = module.ctags.common_tags
   developers_group = local.developers_group
+  project          = var.project
 }
 module "ctags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
