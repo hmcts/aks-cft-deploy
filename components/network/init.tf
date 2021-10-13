936e6c9d70c4a494a6b9a443a5b8acff6133ee57
Ciarán Carragher <c.carragher@kainos.com>
2021-10-13 20:30:45 +0100

    Adds preview sub ID to locals for core-infra-routetable

--- components/network/init.tf
+++ components/network/init.tf
@@ -97,7 +97,10 @@ locals {
     ptlsbox = {
       subscription = "1497c3d7-ab6d-4bb7-8a10-b51d03189ee3"
     }
+    preview = {
+      subscription = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
+    }
   }
 
-  environment = var.environment == "sbox" ? "sandbox" : var.environment == "test" ? "perftest" : "${var.environment}"
+  environment = var.environment == "sbox" ? "sandbox" : var.environment == "test" ? "perftest" : var.environment == "preview" ? "dev" "${var.environment}"
 }
