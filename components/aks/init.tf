11291530b71d1e6de164ae6b9ed29415866d8d28
Ciarán Carragher <c.carragher@kainos.com>
2021-10-13 22:14:19 +0100

    Add locals preview mapping back in

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -21,9 +21,9 @@ locals {
 
   control_resource_environment = var.environment == "perftest" ? "test" : var.environment == "aat" ? "stg" : var.environment == "ptlsbox" ? "sbox" : var.environment == "preview" ? "dev" : "${var.environment}"
 
-  environment = var.environment == "sbox" ? "sandbox" : var.environment == "test" ? "perftest" : var.environment == "ptlsbox" ? "cftsbox-intsvc" : "${var.environment}"
+  environment = var.environment == "sbox" ? "sandbox" : var.environment == "test" ? "perftest" : var.environment == "ptlsbox" ? "cftsbox-intsvc" : var.environment == "preview" ? "dev" : "${var.environment}"
 
-  environment-mi = var.environment == "sandbox" ? "sbox" : var.environment == "test" ? "perftest" : var.environment == "aat" ? "stg" : "${var.environment}"
+  environment-mi = var.environment == "sandbox" ? "sbox" : var.environment == "test" ? "perftest" : var.environment == "aat" ? "stg" : var.environment == "preview" ? "dev" : "${var.environment}"
   acr = {
     global = {
       subscription = "8999dec3-0104-4a27-94ee-6588559729d1"
