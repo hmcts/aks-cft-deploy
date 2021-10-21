e9619b05afb2046664dded278ee34340c83fe6d5
Darren Bromwich <d.bromwich@kainos.com>
2021-10-21 16:56:25 +0100

    updated cidr ranges for demo env

--- components/network/network.tf
+++ components/network/network.tf
@@ -31,16 +31,15 @@ module "ctags" {
   builtFrom   = var.builtFrom
 }
 
-resource "azurerm_public_ip" "demo_public_ip" {
-  count = contains(["demo"], var.environment) ? 0 : 2
-  name = format("aks-%s-pip",
-    var.service_shortname,
-    var.environment
-  )
+# resource "azurerm_public_ip" "demo_public_ip" {
+#   count = contains(["demo"], var.environment) ? 0 : 2
+#   name = format("aks-%s-pip",
+#     var.environment
+#   )
 
-  location            = var.location
-  resource_group_name = "core-infra-${local.environment}"
-  allocation_method   = "Static"
+#   location            = var.location
+#   resource_group_name = "core-infra-${local.environment}"
+#   allocation_method   = "Static"
 
-  tags = module.ctags.common_tags
-}
+#   tags = module.ctags.common_tags
+# }
