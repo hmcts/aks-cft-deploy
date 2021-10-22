2cc40b4a4170de785d4a3464e31f076d792e583b
Darren Bromwich <d.bromwich@kainos.com>
2021-10-22 13:42:58 +0100

    updated genesis branch

--- components/genesis/genesis.tf
+++ components/genesis/genesis.tf
@@ -1,5 +1,5 @@
 module "genesis" {
-  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=cftsds"
+  source           = "git::https://github.com/hmcts/aks-module-genesis.git?ref=master"
   environment      = local.environment
   tags             = module.ctags.common_tags
   developers_group = local.developers_group
