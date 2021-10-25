f156a2deb34ce4d4dca0ef62b0c66d16ac3fffd8
Darren Bromwich <d.bromwich@kainos.com>
2021-10-25 15:55:29 +0100

    update public ip

--- components/network/public-ip.tf
+++ components/network/public-ip.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "demo_public_ip" {
-  count = contains(["demo"], var.environment) ? 0 : 2
+  count = contains(["demo"], var.environment) ? 2 : 0
   name = format("cft-aks-%s-pip",
     var.environment
   )
