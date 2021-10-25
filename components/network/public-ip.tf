eb7efa9881320bd89351164015539b6e2f2d7f80
Darren Bromwich <d.bromwich@kainos.com>
2021-10-25 16:24:19 +0100

    updated public ip

--- components/network/public-ip.tf
+++ components/network/public-ip.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "demo_public_ip" {
   count = contains(["demo"], var.environment) ? 2 : 0
   name = format("cft-aks-%s-pip",
     var.environment
-  )[count.index]
+  )
 
   location            = var.location
   resource_group_name = "core-infra-${local.environment}"
