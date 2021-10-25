ae79bd0b7e2ac3a61831046005ceb10ae0145bdf
Darren Bromwich <d.bromwich@kainos.com>
2021-10-25 20:40:24 +0100

    updated public ip

--- components/network/public-ip.tf
+++ components/network/public-ip.tf
@@ -1,6 +1,21 @@
-resource "azurerm_public_ip" "cft-aks-demo-pip" {
-  count = contains(["demo"], var.environment) ? 2 : 0
-  name  = azurerm_public_ip.name-[count.index]
+resource "azurerm_public_ip" "demo_public_ip" {
+  count = contains(["demo"], var.environment) ? 1 : 0
+  name = format("cft-aks-%s-pip-01",
+    var.environment
+  )
+
+  location            = var.location
+  resource_group_name = "core-infra-${local.environment}"
+  allocation_method   = "Static"
+
+  tags = module.ctags.common_tags
+}
+
+resource "azurerm_public_ip" "demo_public_ip" {
+  count = contains(["demo"], var.environment) ? 1 : 0
+  name = format("cft-aks-%s-pip-02",
+    var.environment
+  )
 
   location            = var.location
   resource_group_name = "core-infra-${local.environment}"
