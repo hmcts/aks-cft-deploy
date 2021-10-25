1ab883a7c009d3fcac2b1b8d6102bb07049f3962
Darren Bromwich <d.bromwich@kainos.com>
2021-10-25 10:23:00 +0100

    added public ip resource

---
+++ components/network/public-ip.tf
@@ -0,0 +1,12 @@
+resource "azurerm_public_ip" "demo_public_ip" {
+  count = contains(["demo"], var.environment) ? 0 : 2
+  name = format("aks-%s-pip",
+    var.environment
+  )
+
+  location            = var.location
+  resource_group_name = "core-infra-${local.environment}"
+  allocation_method   = "Static"
+
+  tags = module.ctags.common_tags
+}
