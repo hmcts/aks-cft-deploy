ddcfdcd699c701190d3e33965bef1e87a3a544d1
Darren Bromwich <d.bromwich@kainos.com>
2021-10-26 16:28:45 +0100

    enabled uami_cft_rg_identity_operator

--- components/aks/cft_aks.tf
+++ components/aks/cft_aks.tf
@@ -3,15 +3,15 @@ data "azurerm_resource_group" "managed-identity-operator-cft-mi" {
   name     = "managed-identities-${local.environment}-rg"
 }
 
-# resource "azurerm_role_assignment" "uami_cft_rg_identity_operator" {
-#   count                = var.cluster_count
-#   provider             = azurerm.acr
-#   principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
-#   scope                = data.azurerm_resource_group.managed-identity-operator-cft-mi.id
-#   role_definition_name = "Managed Identity Operator"
+resource "azurerm_role_assignment" "uami_cft_rg_identity_operator" {
+  count                = var.cluster_count
+  provider             = azurerm.acr
+  principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
+  scope                = data.azurerm_resource_group.managed-identity-operator-cft-mi.id
+  role_definition_name = "Managed Identity Operator"
 
-#   depends_on = [module.kubernetes]
-# }
+  depends_on = [module.kubernetes]
+}
 
 # data "azurerm_resource_group" "managed-identity-operator-cft" {
 #   name = "managed-identities-${local.environment-mi}-rg"
