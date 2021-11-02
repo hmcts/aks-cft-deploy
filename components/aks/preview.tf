119f6d0821cb1498448730913954ca4c3a455b2c
Thomas Thornton <>
2021-11-02 22:33:41 +0000

    zero for now

--- components/aks/preview.tf
+++ components/aks/preview.tf
@@ -6,7 +6,7 @@ data "azurerm_resource_group" "managed-identity-preview1aat" {
 resource "azurerm_role_assignment" "preview1aat_cft_rg_identity_operator" {
   count                = contains(["preview"], var.environment) ? 1 : 0
   provider             = azurerm.preview1aat
-  principal_id         = module.kubernetes["${var.cluster_count}"].kubelet_object_id
+  principal_id         = module.kubernetes[0].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview1aat.id
   role_definition_name = "Managed Identity Operator"
 
@@ -21,7 +21,7 @@ data "azurerm_resource_group" "managed-identity-preview2aat" {
 resource "azurerm_role_assignment" "preview2aat_cft_rg_identity_operator" {
   count                = contains(["preview"], var.environment) ? 1 : 0
   provider             = azurerm.preview2aat
-  principal_id         = module.kubernetes["${var.cluster_count}"].kubelet_object_id
+  principal_id         = module.kubernetes[0].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview2aat.id
   role_definition_name = "Managed Identity Operator"
 
