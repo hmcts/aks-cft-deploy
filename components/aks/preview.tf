f60c66746796e2079c363d1eac3858515788458b
Thomas Thornton <>
2021-11-02 22:44:03 +0000

    test updated approach

--- components/aks/preview.tf
+++ components/aks/preview.tf
@@ -4,9 +4,9 @@ data "azurerm_resource_group" "managed-identity-preview1aat" {
 }
 
 resource "azurerm_role_assignment" "preview1aat_cft_rg_identity_operator" {
-  count                = contains(["preview"], var.environment) ? 1 : 0
+  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview1aat
-  principal_id         = module.kubernetes[0].kubelet_object_id
+  principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview1aat.id
   role_definition_name = "Managed Identity Operator"
 
@@ -19,9 +19,9 @@ data "azurerm_resource_group" "managed-identity-preview2aat" {
 }
 
 resource "azurerm_role_assignment" "preview2aat_cft_rg_identity_operator" {
-  count                = contains(["preview"], var.environment) ? 1 : 0
+  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview2aat
-  principal_id         = module.kubernetes[0].kubelet_object_id
+  principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview2aat.id
   role_definition_name = "Managed Identity Operator"
 
