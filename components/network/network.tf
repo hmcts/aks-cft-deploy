9c99833a10bd783b5f80e2b2b22e16f7b4c0c644
Darren Bromwich <d.bromwich@kainos.com>
2021-11-10 14:19:03 +0000

    update to network branch name

--- components/network/network.tf
+++ components/network/network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/hmcts/aks-module-network.git?ref=cft"
+  source = "git::https://github.com/hmcts/aks-module-network.git?ref=cft-demo"
 
   resource_group_name = local.network_resource_group_name
 
