1d80611eff8e67e772eca3cca06219e0a070fc17
Darren Bromwich <d.bromwich@kainos.com>
2021-11-12 11:11:00 +0000

    updated resource group on rules

--- components/aks/nsg.tf
+++ components/aks/nsg.tf
@@ -8,41 +8,31 @@ data "azurerm_resources" "aks_nsg" {
   type = "Microsoft.Network/networkSecurityGroups"
 }
 resource "azurerm_network_security_rule" "AllowInternetToOAuthProxy" {
-  count                      = contains(["demo"], var.environment) ? 1 : 0
-  name                       = "AllowInternetToOAuthProxy"
-  priority                   = 100
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "80, 443"
-  source_address_prefix      = "*"
-  destination_address_prefix = "51.11.25.221, 20.68.184.102"
-  resource_group_name = format("%s-%s-%s-%s-node-rg",
-    var.project,
-    var.environment,
-    var.cluster_number,
-    var.service_shortname
-  )
+  count                       = contains(["demo"], var.environment) ? 1 : 0
+  name                        = "AllowInternetToOAuthProxy"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80, 443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "51.11.25.221, 20.68.184.102"
+  resource_group_name         = data.azurerm_resources.aks_nsg.resource_group_name.name
   network_security_group_name = data.azurerm_resources.aks_nsg[count.index].resources.0.name
 }
 
 resource "azurerm_network_security_rule" "TraefikNoProxy" {
-  count                      = contains(["demo"], var.environment) ? 1 : 0
-  name                       = "TraefikNoProxy"
-  priority                   = 110
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "443"
-  source_address_prefix      = "*"
-  destination_address_prefix = "51.11.5.163, 20.68.186.154"
-  resource_group_name = format("%s-%s-%s-%s-node-rg",
-    var.project,
-    var.environment,
-    var.cluster_number,
-    var.service_shortname
-  )
+  count                       = contains(["demo"], var.environment) ? 1 : 0
+  name                        = "TraefikNoProxy"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "51.11.5.163, 20.68.186.154"
+  resource_group_name         = data.azurerm_resources.aks_nsg.resource_group_name.name
   network_security_group_name = data.azurerm_resources.aks_nsg[count.index].resources.0.name
 }
