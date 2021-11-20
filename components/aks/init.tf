b02c6b2003fdb383074754602138d2dce8f7c5e4
Ciaran <c.carragher@kainos.com>
2021-11-20 13:54:14 +0000

    Fix tf version and update provider version

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.0.11"
 
   backend "azurerm" {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "2.77.0"
+      version               = "2.86.0"
       configuration_aliases = [azurerm.hmcts-control]
     }
   }
