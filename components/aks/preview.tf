8674019d0d2a1527e679207e52628c99bca2d85d
thomast1906 <thomast@kainos.com>
2021-12-16 12:20:32 +0000

    comment out depends on

--- components/aks/preview.tf
+++ components/aks/preview.tf
@@ -4,14 +4,13 @@ data "azurerm_resource_group" "managed-identity-preview1aat" {
 }
 
 resource "azurerm_role_assignment" "preview1aat_cft_rg_identity_operator" {
-  # count                = (contains(["PREVIEW"], var.environment) ? 1 : 0) * var.cluster_count
-  count                = var.cluster_count
+  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview1aat
   principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview1aat.id
   role_definition_name = "Managed Identity Operator"
 
-  depends_on = [module.kubernetes]
+  # depends_on = [module.kubernetes]
 }
 
 data "azurerm_resource_group" "managed-identity-preview2aat" {
@@ -20,12 +19,11 @@ data "azurerm_resource_group" "managed-identity-preview2aat" {
 }
 
 resource "azurerm_role_assignment" "preview2aat_cft_rg_identity_operator" {
-  # count                = (contains(["PREVIEW"], var.environment) ? 1 : 0) * var.cluster_count
-  count                = var.cluster_count
+  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview2aat
   principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview2aat.id
   role_definition_name = "Managed Identity Operator"
 
-  depends_on = [module.kubernetes]
+  # depends_on = [module.kubernetes]
 }
