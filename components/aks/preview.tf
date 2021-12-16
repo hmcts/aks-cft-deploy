d3b605bb28d020e6f2d389516b4f5bc75ad0ab7c
thomast1906 <thomast@kainos.com>
2021-12-16 11:57:36 +0000

    preview test

--- components/aks/preview.tf
+++ components/aks/preview.tf
@@ -4,7 +4,7 @@ data "azurerm_resource_group" "managed-identity-preview1aat" {
 }
 
 resource "azurerm_role_assignment" "preview1aat_cft_rg_identity_operator" {
-  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
+  count                = (contains(["PREVIEW"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview1aat
   principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview1aat.id
@@ -19,7 +19,7 @@ data "azurerm_resource_group" "managed-identity-preview2aat" {
 }
 
 resource "azurerm_role_assignment" "preview2aat_cft_rg_identity_operator" {
-  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
+  count                = (contains(["PREVIEW"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview2aat
   principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
   scope                = data.azurerm_resource_group.managed-identity-preview2aat.id
