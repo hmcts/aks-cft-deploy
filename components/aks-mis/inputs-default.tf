ec76bcdb7c28a8cc3d9b70ff86ceb53117ad4726
thomast1906 <thomast@kainos.com>
2022-01-06 13:57:49 +0000

    not needed

--- components/aks-mis/inputs-default.tf
+++ components/aks-mis/inputs-default.tf
@@ -9,15 +9,3 @@ variable "location" {
 variable "service_shortname" {
   default = "aks"
 }
-
-variable "system_node_pool" {
-  description = "Map to override the system node pool config"
-}
-
-variable "linux_node_pool" {
-  description = "Map to override the linux node pool config"
-}
-
-variable "kubernetes_cluster_agent_max_pods" {
-  default = "30"
-}
