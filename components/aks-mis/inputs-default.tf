59ddb866fe86b0be7cd177959ac87cfc1f329dd2
thomast1906 <thomast@kainos.com>
2022-01-06 13:10:16 +0000

    Test against demo

---
+++ components/aks-mis/inputs-default.tf
@@ -0,0 +1,23 @@
+locals {
+
+}
+
+variable "location" {
+  default = "uksouth"
+}
+
+variable "service_shortname" {
+  default = "aks"
+}
+
+variable "system_node_pool" {
+  description = "Map to override the system node pool config"
+}
+
+variable "linux_node_pool" {
+  description = "Map to override the linux node pool config"
+}
+
+variable "kubernetes_cluster_agent_max_pods" {
+  default = "30"
+}
--- components/aks-mis/preview.tf
+++ components/aks-mis/preview.tf
@@ -6,11 +6,9 @@ data "azurerm_resource_group" "managed-identity-preview1aat" {
 resource "azurerm_role_assignment" "preview1aat_cft_rg_identity_operator" {
   count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview1aat
-  principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
+  principal_id         = data.azurerm_kubernetes_cluster.kubernetes["${count.index}"].kubelet_identity[0].object_id
   scope                = data.azurerm_resource_group.managed-identity-preview1aat.id
   role_definition_name = "Managed Identity Operator"
-
-  # depends_on = [module.kubernetes]
 }
 
 data "azurerm_resource_group" "managed-identity-preview2aat" {
@@ -21,9 +19,7 @@ data "azurerm_resource_group" "managed-identity-preview2aat" {
 resource "azurerm_role_assignment" "preview2aat_cft_rg_identity_operator" {
   count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.preview2aat
-  principal_id         = module.kubernetes["${count.index}"].kubelet_object_id
+  principal_id         = data.azurerm_kubernetes_cluster.kubernetes["${count.index}"].kubelet_identity[0].object_id
   scope                = data.azurerm_resource_group.managed-identity-preview2aat.id
   role_definition_name = "Managed Identity Operator"
-
-  # depends_on = [module.kubernetes]
 }
