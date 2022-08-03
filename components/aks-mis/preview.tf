356f4d6bd535e8f9ffc7511d9a584e7b88fd6a89
Hannah Smith <hannahs@kainos.com>
2022-08-03 10:04:49 +0100

    fix

--- components/aks-mis/preview.tf
+++ components/aks-mis/preview.tf
@@ -24,10 +24,15 @@ resource "azurerm_role_assignment" "preview2aat_cft_rg_identity_operator" {
   role_definition_name = "Managed Identity Operator"
 }
 
+data "azurerm_user_assigned_identity" "preview_sops_mi" {
+  name                = "aks-preview-mi"
+  resource_group_name = "genesis-rg"
+}
+
 resource "azurerm_role_assignment" "preview_externaldns_read_rg" {
   count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.dts-cftptl-intsvc
-  principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
+  principal_id         = data.azurerm_user_assigned_identity.preview_sops_mi.principal_id
   scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg"
   role_definition_name = "Reader"
 }
@@ -35,7 +40,7 @@ resource "azurerm_role_assignment" "preview_externaldns_read_rg" {
 resource "azurerm_role_assignment" "preview_externaldns_dns_zone_contributor" {
   count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
   provider             = azurerm.dts-cftptl-intsvc
-  principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
+  principal_id         = data.azurerm_user_assigned_identity.preview_sops_mi.principal_id
   scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/service.core-compute-preview.internal"
   role_definition_name = "Private DNS Zone Contributor"
 }
