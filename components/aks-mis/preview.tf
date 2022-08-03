a8a44886ba36d541e10e22e1ea180d911c85ac58
Hannah Smith <13995068+hannah38@users.noreply.github.com>
2022-08-03 15:57:09 +0100

    External dns perms - Preview (#234)

--- components/aks-mis/preview.tf
+++ components/aks-mis/preview.tf
@@ -1,3 +1,5 @@
+# Update -target in azure-pipelines.yaml
+
 data "azurerm_resource_group" "managed-identity-preview1aat" {
   provider = azurerm.preview1aat
   name     = "managed-identities-aat-rg"
@@ -22,4 +24,26 @@ resource "azurerm_role_assignment" "preview2aat_cft_rg_identity_operator" {
   principal_id         = data.azurerm_kubernetes_cluster.kubernetes["${count.index}"].kubelet_identity[0].object_id
   scope                = data.azurerm_resource_group.managed-identity-preview2aat.id
   role_definition_name = "Managed Identity Operator"
+}
+
+data "azurerm_user_assigned_identity" "preview_sops_mi" {
+  provider            = azurerm.preview
+  name                = "aks-preview-mi"
+  resource_group_name = "genesis-rg"
+}
+
+resource "azurerm_role_assignment" "preview_externaldns_read_rg" {
+  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
+  provider             = azurerm.dts-cftptl-intsvc
+  principal_id         = data.azurerm_user_assigned_identity.preview_sops_mi.principal_id
+  scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg"
+  role_definition_name = "Reader"
+}
+
+resource "azurerm_role_assignment" "preview_externaldns_dns_zone_contributor" {
+  count                = (contains(["preview"], var.environment) ? 1 : 0) * var.cluster_count
+  provider             = azurerm.dts-cftptl-intsvc
+  principal_id         = data.azurerm_user_assigned_identity.preview_sops_mi.principal_id
+  scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/service.core-compute-preview.internal"
+  role_definition_name = "Private DNS Zone Contributor"
 }
