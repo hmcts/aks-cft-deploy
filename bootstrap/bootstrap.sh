296e731f1fd1bb6f6576aaea1680c92c0fe4488b
Ciaran <c.carragher@kainos.com>
2022-10-05 15:33:46 +0100

    Comment out steps other then dynatrace registration in bootstrap

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -39,14 +39,14 @@ for cluster in ${6}; do
   set -- "${@:1:5}" "$cluster" "${@:7:9}"
 
   echo "Starting Deployment"
-  ./scripts/get-aks-credentials.sh "$@" || error_exit "ERROR: Unable to get AKS credentials"
-  ./scripts/create-custom-namespaces.sh "$@" || error_exit "ERROR: Unable to create custom namespaces"
-  ./scripts/create-cluster-admins.sh "$@" || error_exit "ERROR: Unable to create cluster admins"
-  ./scripts/install-sealed-secrets.sh "$@"|| error_exit "ERROR: Unable to install sealed secrets"
-  ./scripts/create-flux-githubkey-secret.sh "$@"|| error_exit "ERROR: Unable to create flux githubkey secret"
-  ./scripts/install-flux.sh "$@"|| error_exit "ERROR: Unable to install flux"
-  [[ $6 =~ ^(aat|ithc|perftest|prod)$ ]] && (./scripts/create-neuvector-azure-file-share.sh "$@"|| error_exit "ERROR: Unable to create Neuvector Azure File Shares")
-  [ $9 == "true" ] && (./scripts/generate-sealed-secrets-pki.sh "$@" || error_exit "ERROR: Unable to generate sealed secrets")
+   ./scripts/get-aks-credentials.sh "$@" || error_exit "ERROR: Unable to get AKS credentials"
+  # ./scripts/create-custom-namespaces.sh "$@" || error_exit "ERROR: Unable to create custom namespaces"
+  # ./scripts/create-cluster-admins.sh "$@" || error_exit "ERROR: Unable to create cluster admins"
+  # ./scripts/install-sealed-secrets.sh "$@"|| error_exit "ERROR: Unable to install sealed secrets"
+  # ./scripts/create-flux-githubkey-secret.sh "$@"|| error_exit "ERROR: Unable to create flux githubkey secret"
+  # ./scripts/install-flux.sh "$@"|| error_exit "ERROR: Unable to install flux"
+  # [[ $6 =~ ^(aat|ithc|perftest|prod)$ ]] && (./scripts/create-neuvector-azure-file-share.sh "$@"|| error_exit "ERROR: Unable to create Neuvector Azure File Shares")
+  # [ $9 == "true" ] && (./scripts/generate-sealed-secrets-pki.sh "$@" || error_exit "ERROR: Unable to generate sealed secrets")
   [[ $3 =~ ^(aat|perftest|prod)$ ]] && (./scripts/register-cluster-with-dynatrace.sh "$@" || error_exit "ERROR: Unable to register cluster with Dynatrace")
   echo "Deployment Complete"
 
