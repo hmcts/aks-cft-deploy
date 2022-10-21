99f7181cde6d2f1efcb71589d21141c640985835
reespozzi <rees.pozzi@hmcts.net>
2022-10-21 16:51:24 +0100

    Tidy code

--- components/aks-mis/init.tf
+++ components/aks-mis/init.tf
@@ -112,13 +112,6 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  alias                      = "cft-demo"
-  skip_provider_registration = "true"
-  features {}
-  subscription_id = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
-}
-
-provider "azurerm" {
   alias                      = "reformmgmt"
   skip_provider_registration = "true"
   features {}
