519d68f78cce59f417464b905fa2a4fbb42144f1
reespozzi <rees.pozzi@hmcts.net>
2022-10-25 12:13:27 +0100

    Refactor to save duplication, add count, update pipeline

--- components/aks-mis/demo.tf
+++ components/aks-mis/demo.tf
@@ -1,51 +1,39 @@
 # Update -target in azure-pipelines.yaml
-
-data "azurerm_resource_group" "managed-identity-demo" {
-  provider = azurerm.demo
-  name     = "managed-identities-demo-rg"
-}
-
-resource "azurerm_role_assignment" "demo_cft_rg_identity_operator" {
-  count                = (contains(["demo"], var.environment) ? 1 : 0) * var.cluster_count
-  provider             = azurerm.demo
-  principal_id         = data.azurerm_kubernetes_cluster.kubernetes["${count.index}"].kubelet_identity[0].object_id
-  scope                = data.azurerm_resource_group.managed-identity-demo.id
-  role_definition_name = "Managed Identity Operator"
-}
-
 data "azurerm_user_assigned_identity" "sops_demo_mi" {
   name                = "aks-${var.environment}-mi"
   resource_group_name = "genesis-rg"
 }
 
-resource "azurerm_role_assignment" "demo_externaldns_read_rg" {
+locals {
+  external_dns      = {
+    # Resource Groups to add Reader permissions for external dns to
+    resource_groups = toset([
+      "/subscriptions/ed302caf-ec27-4c64-a05e-85731c3ce90e/resourceGroups/reformMgmtRG",
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg"
+    ])
+    # Demo DNS zones to add "DNS Zone Contributor" premissions for external dns to
+    demo            = toset([
+      "/subscriptions/ed302caf-ec27-4c64-a05e-85731c3ce90e/resourceGroups/reformMgmtRG/providers/Microsoft.Network/dnszones/demo.platform.hmcts.net",
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/demo.platform.hmcts.net",
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/service.core-compute-demo.internal",
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/service.core-compute-idam-demo.internal"
+    ])
+  }
+}
+
+resource "azurerm_role_assignment" "externaldns-dns-zone-contributor" {
+  for_each             = lookup(local.external_dns, var.environment, toset([]))
   count                = (contains(["demo"], var.environment) ? 1 : 0) * var.cluster_count
-  provider             = azurerm.dts-cftptl-intsvc
-  principal_id         = data.azurerm_user_assigned_identity.sops_demo_mi.principal_id
-  scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg"
+  scope                = each.value
+  role_definition_name = contains(regex("^.*/Microsoft.Network/(.*)/.*$", each.value), "privateDnsZones") ? "Private DNS Zone Contributor" : "DNS Zone Contributor"
+  principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
+}
+
+resource "azurerm_role_assignment" "externaldns-read-rg" {
+  # Only add the reader role if there are zones configured
+  for_each             = lookup(local.external_dns, var.environment, null) != null ? local.external_dns.resource_groups : toset([])
+  count                = (contains(["demo"], var.environment) ? 1 : 0) * var.cluster_count
+  scope                = each.value
   role_definition_name = "Reader"
-}
-
-resource "azurerm_role_assignment" "demo_externaldns_private_dns_zone_contributor" {
-  count                = (contains(["demo"], var.environment) ? 1 : 0) * var.cluster_count
-  provider             = azurerm.dts-cftptl-intsvc
-  principal_id         = data.azurerm_user_assigned_identity.sops_demo_mi.principal_id
-  scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/demo.platform.hmcts.net"
-  role_definition_name = "Private DNS Zone Contributor"
-}
-
-resource "azurerm_role_assignment" "demo_externaldns_read_rg_reform" {
-  count                = (contains(["demo"], var.environment) ? 1 : 0) * var.cluster_count
-  provider             = azurerm.reformmgmt
-  principal_id         = data.azurerm_user_assigned_identity.sops_demo_mi.principal_id
-  scope                = "/subscriptions/ed302caf-ec27-4c64-a05e-85731c3ce90e/resourceGroups/reformMgmtRG"
-  role_definition_name = "Reader"
-}
-
-resource "azurerm_role_assignment" "demo_externaldns_dns_zone_contributor_reform" {
-  count                = (contains(["demo"], var.environment) ? 1 : 0) * var.cluster_count
-  provider             = azurerm.reformmgmt
-  principal_id         = data.azurerm_user_assigned_identity.sops_demo_mi.principal_id
-  scope                = "/subscriptions/ed302caf-ec27-4c64-a05e-85731c3ce90e/resourceGroups/reformMgmtRG/providers/Microsoft.Network/dnszones/demo.platform.hmcts.net"
-  role_definition_name = "DNS Zone Contributor"
+  principal_id         = azurerm_user_assigned_identity.sops-mi.principal_id
 }
