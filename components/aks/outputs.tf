9271b16f32444f9bde5aad01eba7cdce1d611d93
Ciaran <c.carragher@kainos.com>
2022-10-27 21:08:44 +0100

    Remove assignment and federated credential code

--- components/aks/outputs.tf
+++ components/aks/outputs.tf
@@ -7,8 +7,3 @@ output "kubelet_object_ids" {
   value     = join(",", module.kubernetes[*].kubelet_object_id)
   sensitive = false
 }
-
-output "oidc_issuer_url" {
-  value     = join(",", module.kubernetes[*].oidc_issuer_url)
-  sensitive = false
-}
