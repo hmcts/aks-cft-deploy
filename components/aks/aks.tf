e1403c6143bcc3f49ca275b8bd2e8d95b2a65ad6
Ciaran <c.carragher@kainos.com>
2022-11-01 11:54:48 +0000

    Change aks module branch back to master?

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -23,7 +23,7 @@ module "loganalytics" {
 
 module "kubernetes" {
   count  = var.cluster_count
-  source = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=DTSPO-10591-oidc-issuer-url"
+  source = "git::https://github.com/hmcts/aks-module-kubernetes.git?ref=master"
 
   control_resource_group = "azure-control-${local.control_resource_environment}-rg"
   environment            = var.environment
