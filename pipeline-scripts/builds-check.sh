8d4db0042f967a4fac47d661a10fd98f77b11648
mokainos <72137340+mokainos@users.noreply.github.com>
2023-02-01 22:01:06 +0000

    Update builds-check.sh

--- pipeline-scripts/builds-check.sh
+++ pipeline-scripts/builds-check.sh
@@ -5,9 +5,10 @@ project="CNP"
 
 echo "This is build $thisbuild"
 IFS=$'\n'
-JSON_DATA=($(curl -s -u :"$azuredevopstoken" --request GET "https://dev.azure.com/$organization/$project/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition" -H "Content-Type: application/json" | jq  '.value[] | .status + (.id|tostring)' | sort -u | grep inProgress))
 test=$(curl -s -u :"$azuredevopstoken" --request GET "https://dev.azure.com/$organization/$project/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition" -H "Content-Type: application/json")
 echo $test
+JSON_DATA=($(curl -s -u :"$azuredevopstoken" --request GET "https://dev.azure.com/$organization/$project/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition" -H "Content-Type: application/json" | jq  '.value[] | .status + (.id|tostring)' | sort -u | grep inProgress))
+
 buildnumber=(${JSON_DATA//[!0-9]/})
 echo $buildnumber
 
