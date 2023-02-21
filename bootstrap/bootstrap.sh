e11b73733c0f73e102c5a0270b1810f2421d8acc
mokainos <72137340+mokainos@users.noreply.github.com>
2023-02-21 08:47:49 +0000

    DTSPO-12422 (#382)

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PARAM_LIST=( PROJECT SERVICE ENVIRONMENT KEYVAULT SUBSCRIPTION_NAME CLUSTER_NAMES COMMAND AKS_KEYVAULT GENERATESEALEDSECRETS )
+PARAM_LIST=( PROJECT SERVICE ENVIRONMENT KEYVAULT SUBSCRIPTION_NAME CLUSTER_NAMES COMMAND AKS_KEYVAULT )
 
 function usage() {
     echo ""
@@ -16,8 +16,8 @@ function error_exit {
 }
 
 
-# Make sure the 9 arguments are passed
-if [[ $# -lt 9 ]]
+# Make sure the 8 arguments are passed
+if [[ $# -lt 8 ]]
 then
     usage
 fi
@@ -36,17 +36,15 @@ chmod +x scripts/create-neuvector-azure-file-share.sh
 chmod +x scripts/cleanup-sshkeys.sh
 
 for cluster in ${6}; do 
-  set -- "${@:1:5}" "$cluster" "${@:7:9}"
+  set -- "${@:1:5}" "$cluster" "${@:7:8}"
 
   echo "Starting Deployment"
   ./scripts/get-aks-credentials.sh "$@" || error_exit "ERROR: Unable to get AKS credentials"
   ./scripts/create-custom-namespaces.sh "$@" || error_exit "ERROR: Unable to create custom namespaces"
   ./scripts/create-cluster-admins.sh "$@" || error_exit "ERROR: Unable to create cluster admins"
-  ./scripts/install-sealed-secrets.sh "$@"|| error_exit "ERROR: Unable to install sealed secrets"
   ./scripts/create-flux-githubkey-secret.sh "$@"|| error_exit "ERROR: Unable to create flux githubkey secret"
   ./scripts/install-flux.sh "$@"|| error_exit "ERROR: Unable to install flux"
   [[ $6 =~ ^(aat|ithc|perftest|prod)$ ]] && (./scripts/create-neuvector-azure-file-share.sh "$@"|| error_exit "ERROR: Unable to create Neuvector Azure File Shares")
-  [ $9 == "true" ] && (./scripts/generate-sealed-secrets-pki.sh "$@" || error_exit "ERROR: Unable to generate sealed secrets")
   echo "Cleanup"
   ./scripts/cleanup-sshkeys.sh "$@" || error_exit "ERROR: Unable to Cleanup"
   echo "Deployment Complete"
