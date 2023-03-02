0649e058fb7b9b1dc797fda329bb64daf6253bec
Rees Pozzi <reespozzi@icloud.com>
2023-03-02 17:07:55 +0000

    Specify subscription data source attribute

--- components/managed-identity/managed-identity.tf
+++ components/managed-identity/managed-identity.tf
@@ -110,7 +110,7 @@ resource "azurerm_role_assignment" "service_operator" {
   count                = var.service_operator_settings_enabled ? 1 : 0
   principal_id         = data.azurerm_user_assigned_identity.aks.principal_id
   role_definition_name = "Contributor"
-  scope                = data.azurerm_subscription.subscription
+  scope                = data.azurerm_subscription.subscription.id
 }
 
 module "ctags" {
