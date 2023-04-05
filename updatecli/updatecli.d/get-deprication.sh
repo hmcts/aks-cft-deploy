f9201ab4f2e7bb196a4e830d36f33577163dd0e3
mananpatel26 <manan.patel@kainos.com>
2023-04-05 15:35:35 +0100

    Adding pluto run in action and script

---
+++ updatecli/updatecli.d/get-deprication.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+environment="$CURRENT_ITER_ENVIRONMENT"
+aks_name=`yq ".environments.$environment.aks_name" ./updatecli/values.github-action.yaml`
+aks_resource_group=`yq ".environments.$environment.aks_resource_group" ./updatecli/values.github-action.yaml`
+az account set $aks_subscription
+
+aks_name=$1
+aks_resource_group=$2
+aks_subscription=$3
+environment=$4
+
+set -x
+
+{
+printf "\n\nTrying cluster $aks_name $aks_resource_group\n"
+az aks get-credentials \
+    --resource-group $aks_resource_group \
+    --name $aks_name --admin
+$(kubectl get pods)
+pluto detect-helm -owide
+} || {
+aks_resource_group=$(echo $aks_resource_group)
+aks_name=$(echo $aks_name)
+printf "\n\nTrying cluster $aks_name $aks_resource_group\n"
+az aks get-credentials \
+    --resource-group $aks_resource_group \
+    --name $aks_name --admin
+}
