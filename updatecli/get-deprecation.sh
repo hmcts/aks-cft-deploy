6de3bbf0b1fb608f08a74277f899b6cc7a653d8d
mananpatel26 <manan.patel@kainos.com>
2023-04-11 15:28:13 +0100

    Adding subscription field in script

--- updatecli/get-deprecation.sh
+++ updatecli/get-deprecation.sh
@@ -5,7 +5,7 @@ environment="$CURRENT_ITER_ENVIRONMENT"
 aks_name=`yq ".environments.$environment.aks_name" ./updatecli/values.github-action.yaml`
 aks_resource_group=`yq ".environments.$environment.aks_resource_group" ./updatecli/values.github-action.yaml`
 aks_subscription=`yq ".environments.$environment.aks_subscription" ./updatecli/values.github-action.yaml`
-az account set -s $aks_subscription
+az account set -s "${aks_subscription}"
 
 aks_name=$1
 aks_resource_group=$2
@@ -15,10 +15,10 @@ environment=$4
 set -x
 
 {
-printf "\n\nTrying cluster $aks_name $aks_resource_group\n"
+echo "Trying cluster ${aks_name} in RG ${aks_resource_group}"
 az aks get-credentials \
-    --resource-group $aks_resource_group \
-    --name $aks_name --admin
+    --resource-group "${aks_resource_group}"\
+    --name "${aks_name}" --admin
 $(kubectl get pods)
 pluto detect-helm -owide
 }
