1bba70b93e3437e38f0b7f113c3a4b3548b07b83
mananpatel26 <manan.patel@kainos.com>
2023-04-11 15:51:04 +0100

    Adding subscription field in script

--- updatecli/get-deprecation.sh
+++ updatecli/get-deprecation.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-
 environment="$CURRENT_ITER_ENVIRONMENT"
 aks_name=`yq ".environments.$environment.aks_name" ./updatecli/values.github-action.yaml`
 aks_resource_group=`yq ".environments.$environment.aks_resource_group" ./updatecli/values.github-action.yaml`
 aks_subscription=`yq ".environments.$environment.aks_subscription" ./updatecli/values.github-action.yaml`
 
+set -x 
+{
 echo "Before-------"
 echo "${aks_name}"
 echo "${aks_resource_group}"
@@ -13,8 +14,6 @@ echo "${aks_subscription}"
 echo "${environment}"
 
 az account set -s "${aks_subscription}"
-
-
 
 aks_name=$1
 aks_resource_group=$2
@@ -26,9 +25,7 @@ echo "${aks_name}"
 echo "${aks_resource_group}"
 echo "${aks_subscription}"
 echo "${environment}"
-set -x
 
-{
 echo "Trying cluster ${aks_name} in RG ${aks_resource_group}"
 az aks get-credentials \
     --resource-group "${aks_resource_group}"\
