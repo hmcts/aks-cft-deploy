2a5af357f4f82a85c51efe92be617f3fb7bfd89a
mananpatel26 <manan.patel@kainos.com>
2023-04-14 15:31:48 +0100

    Altering Pluto script

--- updatecli/get-deprecation.sh
+++ updatecli/get-deprecation.sh
@@ -5,17 +5,26 @@ aks_name=`yq ".environments.$environment.aks_name" ./updatecli/values.github-act
 aks_resource_group=`yq ".environments.$environment.aks_resource_group" ./updatecli/values.github-action.yaml`
 aks_subscription=`yq ".environments.$environment.aks_subscription" ./updatecli/values.github-action.yaml`
 
+# get kubelogin
+wget https://github.com/Azure/kubelogin/releases/download/v0.0.9/kubelogin-linux-amd64.zip
+unzip kubelogin-linux-amd64.zip
+sudo mv bin/linux_amd64/kubelogin /usr/bin
+kubelogin convert-kubeconfig -l azurecli
+
 echo "Before-------"
 echo "${aks_name}"
 echo "${aks_resource_group}"
 echo "${aks_subscription}"
 echo "${environment}"
-#kubelogin --version
+
+kubelogin --version
+
 az account set -s "${aks_subscription}"
 
 echo "Trying cluster ${aks_name} in RG ${aks_resource_group}"
 az aks get-credentials \
     --resource-group "${aks_resource_group}"\
     --name "${aks_name}"
+
 #$(kubectl get pods)
 pluto detect-helm -o yaml -v 5 --target-versions k8s=v1.26.0
