f480097b3ed7ba006bcf4171ddb3f18d3fe52cfa
Ciaran <ciaran.carragher2@justice.gov.uk>
2023-06-06 14:58:56 +0100

    Adds var sub id to provider

--- components/aks/init.tf
+++ components/aks/init.tf
@@ -13,8 +13,12 @@ terraform {
   }
 }
 
+variable "subscription_id" {}
+
 provider "azurerm" {
   features {}
+  subscription_id            = var.subscription_id
+  skip_provider_registration = "true"
 }
 
 locals {
