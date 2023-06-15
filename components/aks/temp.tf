803d3d7d2a38313885fbb2b56d50a16e8220968f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-15 14:48:51 +0100

    Update temp.tf

--- components/aks/temp.tf
+++ components/aks/temp.tf
@@ -2,13 +2,3 @@ data "azurerm_resource_group" "managed-identity-operator-cft-mi" {
   provider = azurerm.acr
   name     = "managed-identities-${local.environment}-rg"
 }
-
-data "azurerm_resource_group" "managed-identity-preview1aat" {
-  provider = azurerm.preview1aat
-  name     = "managed-identities-aat-rg"
-}
-
-data "azurerm_resource_group" "managed-identity-preview2aat" {
-  provider = azurerm.preview2aat
-  name     = "managed-identities-aat-rg"
-}
