b34cf031a2fa34028d19feec40a423ab361d7b1f
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-08-15 07:57:17 +0100

    Test ASO installation

--- bootstrap/scripts/install-flux.sh
+++ bootstrap/scripts/install-flux.sh
@@ -3,7 +3,7 @@ set -ex
 
 ENV=$3
 CLUSTER_NAME=$6
-FLUX_CONFIG_URL=https://raw.githubusercontent.com/hmcts/cnp-flux-config/master
+FLUX_CONFIG_URL=https://raw.githubusercontent.com/hmcts/cnp-flux-config/asoraw
 AGENT_BUILDDIRECTORY=/tmp
 KUSTOMIZE_VERSION=4.5.7
 
@@ -52,7 +52,7 @@ EOF
 }
 
 function flux_v2_installation {
-    FLUX_CONFIG_URL=https://raw.githubusercontent.com/hmcts/cnp-flux-config/master
+    FLUX_CONFIG_URL=https://raw.githubusercontent.com/hmcts/cnp-flux-config/asoraw
      mkdir -p "${TMP_DIR}/gotk"
 
 # Deploy components and CRDs
@@ -124,7 +124,7 @@ mkdir -p ${TMP_DIR}/gotk
 # Create admin namespace
 create_admin_namespace
 
-#  Create secret in admin namespace 
+#  Create secret in admin namespace
 flux_ssh_git_key
 
 # Deploy AAD Pod Identity
@@ -151,6 +151,19 @@ EOF
 ) > "${TMP_DIR}/kustomization.yaml"
 # -----------------------------------------------------------
 
+./kustomize build ${TMP_DIR} |  kubectl apply -f -
+
+(
+cat <<EOF
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - https://github.com/hmcts/cnp-flux-config//apps/azureserviceoperator-system/cert-manager/?ref=asoraw
+  - https://github.com/hmcts/cnp-flux-config//apps/azureserviceoperator-system/aso/?ref=asoraw
+  - https://raw.githubusercontent.com/hmcts/cnp-flux-config/asoraw/apps/azureserviceoperator-system/sbox/base/aso-controller-settings.yaml
+EOF
+) > "${TMP_DIR}/kustomization.yaml"
+# -----------------------------------------------------------
 ./kustomize build ${TMP_DIR} |  kubectl apply -f -
 
 rm -rf kustomize
