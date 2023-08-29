3cf1d8f74aeac9a98378875ed9729a1bdd5a867a
Vik <viktoriya.bogdanova@hmcts.net>
2023-08-29 11:23:22 +0100

    Remove data kubernetes block

--- components/aks-mis/cft_aks.tf
+++ components/aks-mis/cft_aks.tf
@@ -6,13 +6,13 @@ data "azurerm_resource_group" "managed-identity-operator-cft-mi" {
 resource "azurerm_role_assignment" "uami_cft_rg_identity_operator" {
   for_each             = toset(var.clusters)
   provider             = azurerm.acr
-  principal_id         = data.azurerm_kubernetes_cluster.kubernetes[each.key].kubelet_identity[0].object_id
+  principal_id         = module.kubernetes[each.key].kubelet_identity[0].object_id
   scope                = data.azurerm_resource_group.managed-identity-operator-cft-mi.id
   role_definition_name = "Managed Identity Operator"
 }
 
-data "azurerm_kubernetes_cluster" "kubernetes" {
-  for_each            = toset(var.clusters)
-  name                = "${var.project}-${var.env}-${each.key}-${var.service_shortname}"
-  resource_group_name = "${var.project}-${var.env}-${each.key}-rg"
-}
+# data "azurerm_kubernetes_cluster" "kubernetes" {
+#   for_each            = toset(var.clusters)
+#   name                = "${var.project}-${var.env}-${each.key}-${var.service_shortname}"
+#   resource_group_name = "${var.project}-${var.env}-${each.key}-rg"
+# }
