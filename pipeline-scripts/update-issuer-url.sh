9601919a1facef41c47ae687cd9c487ee7b1640c
Rees Pozzi <reespozzi@icloud.com>
2023-09-25 15:28:34 +0100

    Show file changes of issuer url

--- pipeline-scripts/update-issuer-url.sh
+++ pipeline-scripts/update-issuer-url.sh
@@ -4,13 +4,22 @@ set -e
 ENV=$1
 CLUSTER=$2
 ISSUER_URL=$3
+REPO=$4
 
-echo "Issuer URL is: ${ISSUER_URL}"
+cd "$REPO"
 
-# Check ISSUER url is set
-
-# log in as account which has access for github
-# Update file in place apps/flux-system/ENV/CLUSTER/kustomize.yaml
 # Update after ISSUER_URL: 
+if [ -n "$ISSUER_URL" ]; then
+    echo "Issuer URL is: ${ISSUER_URL}"
+    #  Make file changes
+    file_path="apps/flux-system/$ENV/$CLUSTER/kustomize.yaml"
+    sed -i '' "s/ISSUER_URL:.*/ISSUER_URL: ${ISSUER_URL}_test/g" $file_path
+
+    # Commit changes to github if there is any
+    if [[ -n $(git status -s) ]]; then 
+        git diff .
+    fi
+fi
+
 
 
