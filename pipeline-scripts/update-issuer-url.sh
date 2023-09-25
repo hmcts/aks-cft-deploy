c7d58320bc9df0c6308d9e431adfc169cce0d1fc
Rees Pozzi <reespozzi@icloud.com>
2023-09-25 13:23:58 +0100

    Remove isoutput - fetch issuer url

---
+++ pipeline-scripts/update-issuer-url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+ENV=$1
+CLUSTER=$2
+ISSUER_URL=$3
+
+echo "Issuer URL is: ${ISSUER_URL}"
+
+# Check ISSUER url is set
+
+# log in as account which has access for github
+# Update file in place apps/flux-system/ENV/CLUSTER/kustomize.yaml
+# Update after ISSUER_URL: 
+
+
