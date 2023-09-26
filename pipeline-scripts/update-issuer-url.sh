1f7bdba779e5935b0f1d3331af839c6fcdeffc6b
Rees Pozzi <reespozzi@icloud.com>
2023-09-26 09:51:57 +0100

    Fix sed command - include speech marks around URL

--- pipeline-scripts/update-issuer-url.sh
+++ pipeline-scripts/update-issuer-url.sh
@@ -12,10 +12,10 @@ cd "$REPO"
 if [ -n "$ISSUER_URL" ]; then
     echo "Issuer URL is: ${ISSUER_URL}"
     #  Make file changes
-    file_path="apps/flux-system/$ENV/$CLUSTER/kustomize.yaml"
+    file_path="apps/flux-system/${ENV}/${CLUSTER}/kustomize.yaml"
     pwd
     ls
-    sed -i -e "s/ISSUER_URL:.*/ISSUER_URL: ${ISSUER_URL}_test/g" $file_path
+    sed -i -e "s/ISSUER_URL:.*/ISSUER_URL: \"$(echo $ISSUER_URL | sed 's/[\/&]/\\&/g')_test\"/g" $file_path
 
     # Commit changes to github if there is any
     if [[ -n $(git status -s) ]]; then 
