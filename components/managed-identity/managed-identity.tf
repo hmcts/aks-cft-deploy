bbfe5e0c968de95efe33c12d58eb4e9ee35ec5be
Daniel Wilson <daniel.wilson@kainos.com>
2023-09-29 16:45:39 +0100

    tf fmt

--- components/managed-identity/managed-identity.tf
+++ components/managed-identity/managed-identity.tf
@@ -77,45 +77,45 @@ locals {
   mi_cft = {
     # DCD-CNP-Sandbox
     sbox = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "bf308a5c-0624-4334-8ff8-8dca9fd43783"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "bf308a5c-0624-4334-8ff8-8dca9fd43783"
     }
     # DCD-CNP-DEV
     aat = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
     }
     demo = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
     }
     preview = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
     }
     # DCD-CNP-QA
     ithc = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c"
     }
     perftest = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c"
     }
     # DCD-CNP-Prod
     prod = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "8999dec3-0104-4a27-94ee-6588559729d1"
     }
     # DTS-CFTSBOX-INTSVC
     ptlsbox = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "1497c3d7-ab6d-4bb7-8a10-b51d03189ee3"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "1497c3d7-ab6d-4bb7-8a10-b51d03189ee3"
     }
     # DTS-CFTPTL-INTSVC
     ptl = {
-      subscription_scope  = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
-      subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+      subscription_scope = "/subscriptions/${local.mi_cft[var.env].subscription_id}"
+      subscription_id    = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
     }
   }
 }
