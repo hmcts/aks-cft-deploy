d8a90ca41b814df447e3ac5b33dc293ebcb46379
Khaled Siddat <102241196+DJ-Khaled@users.noreply.github.com>
2023-10-13 14:53:46 +0100

    DTSPO-14196: Update cluster version for 01 (#560)
    * update cluster version for 01
    
    * removing unused role assignment

--- components/aks-mis/cft_aks.tf
+++ components/aks-mis/cft_aks.tf
@@ -1,16 +1,3 @@
-data "azurerm_resource_group" "managed-identity-operator-cft-mi" {
-  provider = azurerm.acr
-  name     = "managed-identities-${local.environment}-rg"
-}
-
-resource "azurerm_role_assignment" "uami_cft_rg_identity_operator" {
-  for_each             = toset(var.clusters)
-  provider             = azurerm.acr
-  principal_id         = data.azurerm_kubernetes_cluster.kubernetes[each.key].kubelet_identity[0].object_id
-  scope                = data.azurerm_resource_group.managed-identity-operator-cft-mi.id
-  role_definition_name = "Managed Identity Operator"
-}
-
 data "azurerm_kubernetes_cluster" "kubernetes" {
   for_each            = toset(var.clusters)
   name                = "${var.project}-${var.env}-${each.key}-${var.service_shortname}"
