15f35b9651f2d5dfbe8fe33640971072c2aa459b
Rees Pozzi <reespozzi@icloud.com>
2023-10-18 10:19:28 +0100

    Remove data reference

--- components/managed-identity/managed-identity.tf
+++ components/managed-identity/managed-identity.tf
@@ -150,7 +150,7 @@ resource "azurerm_role_assignment" "service_operator_workload_identity" {
 resource "azurerm_role_assignment" "preview_admin_mi_externaldns_read_rg" {
   count                = (contains(["preview"], var.env) ? 1 : 0)
   provider             = azurerm.dts-cftptl-intsvc
-  principal_id         = data.azurerm_user_assigned_identity.wi-admin-mi.principal_id
+  principal_id         = azurerm_user_assigned_identity.wi-admin-mi.principal_id
   scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg"
   role_definition_name = "Reader"
 }
@@ -158,7 +158,7 @@ resource "azurerm_role_assignment" "preview_admin_mi_externaldns_read_rg" {
 resource "azurerm_role_assignment" "preview_admin_mi_externaldns_dns_zone_contributor" {
   count                = (contains(["preview"], var.env) ? 1 : 0)
   provider             = azurerm.dts-cftptl-intsvc
-  principal_id         = data.azurerm_user_assigned_identity.wi-admin-mi.principal_id
+  principal_id         = azurerm_user_assigned_identity.wi-admin-mi.principal_id
   scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/service.core-compute-preview.internal"
   role_definition_name = "Private DNS Zone Contributor"
 }
