6a33b88251dacc3bc2b1021778acb503bb5ccbc8
nicholas chang <nicholasc001@hotmail.com>
2024-03-18 12:01:11 +0000

    edit script

--- scripts/checking-clusters.sh
+++ scripts/checking-clusters.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Check if both '00' and '01' clusters exist
-if az aks list --query "[?contains(name, 'a-prod-00-cluster')]" &>/dev/null az aks list --query "[?contains(name, 'a-prod-01-cluster')]" &>/dev/null; then
+if az aks list --query "[?contains(name, 'a-prod-00-cluster')]" &>/dev/null && \
+   az aks list --query "[?contains(name, 'a-prod-01-cluster')]" &>/dev/null; then
     output="All"
 elif az aks list --query "[?contains(name, 'a-prod-00-cluster')]" &>/dev/null; then
     output="00"
