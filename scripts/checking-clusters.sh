990bf560d05c29ad01bb88c72314e02c6088fe70
nicholas chang <nicholasc001@hotmail.com>
2024-03-18 15:50:16 +0000

    edit checking cluster script

--- scripts/checking-clusters.sh
+++ scripts/checking-clusters.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Check if both '00' and '01' clusters exist
-if az aks list --query "[?contains(name, '${project}-${environment}-00-aks')]" &>/dev/null; then
+if az aks list --query "[?contains(name, '${project}-${environment}-00-aks')]" &>/dev/null && \
    az aks list --query "[?contains(name, '${project}-${environment}-01-aks')]" &>/dev/null; then
     output="All"
-elif az aks list --query "[?contains(name, '${project}-${environment}-00-cluster')]" &>/dev/null; then
+elif az aks list --query "[?contains(name, ${project}-${environment}-00-cluster')]" &>/dev/null; then
     output="00"
 elif az aks list --query "[?contains(name, '${project}-${environment}-01-cluster')]" &>/dev/null; then
     output="01"
 else
     output="None"
 fi
-echo $output
+
+echo "$output" <- this echo needs replacing to output the result into ADO pipeline for use.  
