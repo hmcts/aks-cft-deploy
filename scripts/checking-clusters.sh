ecd36745c39b01f7d0a5281159c9c423a0e36c97
nicholas chang <nicholasc001@hotmail.com>
2024-03-18 11:17:56 +0000

    add script to check both 00 & 01 and edit pipeline

---
+++ scripts/checking-clusters.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if both '00' and '01' clusters exist
+if az aks list --query "[?contains(name, 'a-prod-00-cluster')]" &>/dev/null && \
+   az aks list --query "[?contains(name, 'a-prod-01-cluster')]" &>/dev/null; then
+    output="All"
+elif az aks list --query "[?contains(name, 'a-prod-00-cluster')]" &>/dev/null; then
+    output="00"
+elif az aks list --query "[?contains(name, 'a-prod-01-cluster')]" &>/dev/null; then
+    output="01"
+else
+    output="None"
+fi
+
+echo "$output" <- this echo needs replacing to output the result into ADO pipeline for use.  
