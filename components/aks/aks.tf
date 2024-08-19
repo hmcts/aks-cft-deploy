75e29edda808d532987885b7fd850dcff1cbeb45
Narayn Manoharan <narayn.manoharan@justice.gov.uk>
2024-08-19 13:36:39 +0100

    Adding Taint Instead of Node Label

--- components/aks/aks.tf
+++ components/aks/aks.tf
@@ -121,10 +121,7 @@ module "kubernetes" {
         max_pods  = 30
         os_type   = "Linux"
         os_sku    = "AzureLinux"
-        node_labels = {
-          agentpool = "azurelinux"
-        }
-        node_taints         = []
+        node_taints         = ["custom.azurelinux=true:NoSchedule"]
         enable_auto_scaling = true
         mode                = "User"
       }
