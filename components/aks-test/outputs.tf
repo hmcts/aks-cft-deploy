9712046756df0b9e059d92fe556b56bdd3d6327b
nicholas chang <nicholasc001@hotmail.com>
2025-03-19 12:24:17 +0000

    comment out prod,ptl and aat env

--- components/aks-test/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "clusters" {
-  value     = join(" ", flatten([for cluster in module.kubernetes : cluster[*].cluster]))
-  sensitive = false
-}
-
-output "kubelet_object_ids" {
-  value     = join(",", flatten([for cluster in module.kubernetes : cluster[*].kubelet_object_id]))
-  sensitive = false
-}
--- components/aks-test/temp.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_resource_group" "managed-identity-operator-cft-mi" {
-  provider = azurerm.acr
-  name     = "managed-identities-${local.environment}-rg"
-}
