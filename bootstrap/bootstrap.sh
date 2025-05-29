3dc53815359e458d384cf125063444c0afd38136
Dawid Strozak <dawid.strozak@hmcts.net>
2025-05-29 16:25:08 +0100

    Replace github SSH Deploy key download with github app credentials download Create new github app secret and bootstrap Flux with it instead of SSH Deploy key Update the error handling and message slightly (GitHub app credentials cannot be created by the script)

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -45,7 +45,7 @@ for cluster in ${cluster_numbers}; do
   ./scripts/get-aks-credentials.sh "$@" || error_exit "ERROR: Unable to get AKS credentials"
   ./scripts/create-custom-namespaces.sh "$@" || error_exit "ERROR: Unable to create custom namespaces"
   ./scripts/create-cluster-admins.sh "$@" || error_exit "ERROR: Unable to create cluster admins"
-  ./scripts/create-flux-githubkey-secret.sh "$@"|| error_exit "ERROR: Unable to create flux githubkey secret"
+  ./scripts/create-flux-githubkey-secret.sh "$@"|| error_exit "ERROR: Unable to create flux github app secret"
   ./scripts/install-flux.sh "$@"|| error_exit "ERROR: Unable to install flux"
   [[ $6 =~ ^(aat|ithc|perftest|prod)$ ]] && (./scripts/create-neuvector-azure-file-share.sh "$@"|| error_exit "ERROR: Unable to create Neuvector Azure File Shares")
   echo "Cleanup"
