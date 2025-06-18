28b4b3f6a30d59e3e16e423cb05bfdd673dd4d02
dawidstrozak <123395719+dawidstrozak@users.noreply.github.com>
2025-06-18 11:26:39 +0100

    Revert "Revert "Bootstrap Flux with GitHub App secret instead of SSH Deploy k…"
    This reverts commit e6745bc5707051b1009fac194a8be738564467b9.

--- bootstrap/scripts/install-flux.sh
+++ bootstrap/scripts/install-flux.sh
@@ -127,17 +127,19 @@ function install_aso {
 }
 
 
-function flux_ssh_git_key {
-    ssh-keyscan -t ecdsa-sha2-nistp256 github.com > $AGENT_BUILDDIRECTORY/known_hosts
-    echo " Kubectl Create Secret"
-    kubectl create secret generic git-credentials \
-    --from-file=identity=$AGENT_BUILDDIRECTORY/flux-ssh-git-key \
-    --from-file=identity.pub=$AGENT_BUILDDIRECTORY/flux-ssh-git-key.pub \
-    --from-file=known_hosts=$AGENT_BUILDDIRECTORY/known_hosts \
+function flux_github_app_secret {
+    echo " Kubectl Create GitHub App Secret"
+    kubectl create secret generic github-app-credentials \
+    --from-file=githubAppID=$AGENT_BUILDDIRECTORY/flux-github-app-id \
+    --from-file=githubAppInstallationID=$AGENT_BUILDDIRECTORY/flux-github-app-installation-id \
+    --from-file=githubAppPrivateKey=$AGENT_BUILDDIRECTORY/flux-github-app-private-key \
     --namespace flux-system \
-    --dry-run=client -o yaml > "${TMP_DIR}/gotk/git-credentials.yaml"
+    --dry-run=client -o yaml > "${TMP_DIR}/gotk/github-app-credentials.yaml"
+    # Clean up these secret files - they are not needed anymore and to avoid conflicts on subsequent runs on the same agent
+    rm -f $AGENT_BUILDDIRECTORY/flux-github-app-id
+    rm -f $AGENT_BUILDDIRECTORY/flux-github-app-installation-id
+    rm -f $AGENT_BUILDDIRECTORY/flux-github-app-private-key
 }
-
 
 # Installs flux components using workload identity
 function install_flux {
@@ -158,7 +160,7 @@ kind: Kustomization
 namespace: flux-system
 resources:
   - ${FLUX_CONFIG_URL}/apps/flux-system/base/gotk-components.yaml
-  - git-credentials.yaml 
+  - github-app-credentials.yaml
   - workload-identity-federated-credential.yaml
   - workload-identity-ua-identity.yaml
   - workload-identity-rg.yaml
@@ -211,7 +213,7 @@ install_kustomize
 install_aadpodidentity
 install_aso
 # Install flux components
-flux_ssh_git_key
+flux_github_app_secret
 install_flux
 
 # Cleanup
