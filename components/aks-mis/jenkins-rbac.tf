3f46740ee379a1cd66ee266258e7dd439c06881e
Daniel Alves <daniel.alves@justice.gov.uk>
2026-06-25 10:47:22 +0100

    Grant AAT Jenkins MI cross-env network read

---
+++ components/aks-mis/jenkins-rbac.tf
@@ -0,0 +1,27 @@
+data "azurerm_user_assigned_identity" "jenkins_aat_mi" {
+  count = var.env == "aat" ? 1 : 0
+
+  provider            = azurerm.preview2aat
+  name                = "jenkins-aat-mi"
+  resource_group_name = "managed-identities-aat-rg"
+}
+
+# AAT shared-infrastructure plans can read these cross-environment subnets for
+# storage account firewall allowlists, for example ccd-shared-infrastructure.
+resource "azurerm_role_assignment" "jenkins_aat_mi_ptl_network_reader" {
+  count = var.env == "aat" ? 1 : 0
+
+  provider             = azurerm.dts-cftptl-intsvc
+  principal_id         = data.azurerm_user_assigned_identity.jenkins_aat_mi[0].principal_id
+  scope                = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/cft-ptl-network-rg"
+  role_definition_name = "Reader"
+}
+
+resource "azurerm_role_assignment" "jenkins_aat_mi_preview_network_reader" {
+  count = var.env == "aat" ? 1 : 0
+
+  provider             = azurerm.preview
+  principal_id         = data.azurerm_user_assigned_identity.jenkins_aat_mi[0].principal_id
+  scope                = "/subscriptions/8b6ea922-0862-443e-af15-6056e1c9b9a4/resourceGroups/cft-preview-network-rg"
+  role_definition_name = "Reader"
+}
